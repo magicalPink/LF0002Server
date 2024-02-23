2e8dbcd218d6cab0350fdcc9640198238391ca1b
2903680368 <2903680368@qq.com>
2024-02-23 19:14:03 +0800

    优化注册代码

--- router_handler/user.js
+++ router_handler/user.js
@@ -11,6 +11,8 @@ const addUserSql = `INSERT INTO users SET ?`
 //注册
 exports.regUser = (req, res) => {
     const userInfo = req.body
+    userInfo.avatar = '/image/avatar/avatar1.png'
+    console.log(userInfo)
     if (!userInfo.username || !userInfo.password) {
         // return res.send({ status: 1, message: '用户名或密码不能为空！' })
         return res.cc('用户名或密码不能为空！')
@@ -27,6 +29,7 @@ exports.regUser = (req, res) => {
             userInfo.password = bcrypt.hashSync(userInfo.password, 10)
             db.query(addUserSql, userInfo, (err, results) => {
                 if (err) {
+                    console.log(err)
                     return res.cc(err)
                 } else {
                     return res.cc('注册成功！', 0)
